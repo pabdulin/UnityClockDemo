c08da9b9b2f953be8ee93286b2581940a90acbe7
Petr Abdulin <pabdulin@gmail.com>
2019-12-05 18:45:47 +0700

    Complete variant.

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+    const float degreesPerHour = 360 / 12;
+    const float degreesPerMinuteAndSecond = 360 / 60;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var time = DateTime.Now;
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, time.Minute * degreesPerMinuteAndSecond, 0f);
+        secondsTransform.localRotation =
+            Quaternion.Euler(0f, time.Second * degreesPerMinuteAndSecond, 0f);
+    }
+
+    void Awake()
+    {
+    }
+}
